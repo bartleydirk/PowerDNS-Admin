4bf21afaf3ae70d0665302ace67f003fb07479ab
Dirk Bartley <dbartley@spotx.tv>
2018-01-24 22:18:03 -0700

    key pair exchange close

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@
 
 import json
 import os
-import sys
+# import sys
 import base64
 from pprint import pprint
 from flask import request, jsonify
@@ -10,7 +10,8 @@ from admin_api.crypt import Keypair
 
 from app import app
 # from app.models import User, Domain, History, Setting, DomainSetting
-from app.base import Record
+# pylint: disable=E0401
+from .base import Record
 
 DBGREQUEST = False
 DBGDATA = False
@@ -26,61 +27,69 @@ def getheadervalue(headers, value):
     return retval
 
 
+def show(val):
+    """Solve a wierd pylint issue, and make it easy to silence the output."""
+    print(val)
+
+
 @app.route('/api', methods=['GET', 'POST', 'PATCH'])
 # @login_required
 def api():
     """Let us test the api from a brower so I can debug the damn thing."""
     # first authenticate
-    print "Begin run #########################################\n\n"
+    show("Begin run #########################################\n\n")
 
+    # get the headers we can from the client
     apikey = getheadervalue(request.headers, 'X-API-Key')
-    print 'X-API-Key is %s' % (apikey)
+    show('X-API-Key is %s' % (apikey))
     username = getheadervalue(request.headers, 'X-API-User')
-    print 'X-API-User is %s' % (username)
+    show('X-API-User is %s' % (username))
     b64 = getheadervalue(request.headers, 'X-API-Pubkey')
     user_pubkey = base64.b64decode(b64)
-    print 'X-API-Pubkey is %s' % (user_pubkey)
+    show('X-API-Pubkey is %s' % (user_pubkey))
 
     exepath = os.path.dirname(os.path.realpath(__file__))
     oneup = os.path.abspath(os.path.join(exepath, ".."))
     cnfgfile = '%s/%s' % (oneup, 'serverkeys.cfg')
-    print "server keys cfg file is %s" % (cnfgfile)
+    show("server keys cfg file is %s" % (cnfgfile))
 
-    server_keypair = Keypair(cnfgfile=cnfgfile, keyname='serverkeys')
+    server_keypair = Keypair(cnfgfile=cnfgfile)
     # data_ = server_keypair.encrypt('Hello There')
-    # print 'The decrypted string is %s' % (server_keypair.decrypt(data_))
+    # show('The decrypted string is %s' % (server_keypair.decrypt(data_)))
 
-    client_keypair = Keypair(cnfgfile=cnfgfile, keyname='user_%s' % username)
+    client_keypair = Keypair(cnfgfile=cnfgfile, username=username, pubkeystring=user_pubkey)
 
     if apikey == 'sendserverkey':
         # there is no api key, the client needs one, we have the client public
         # encrypt a token, after getting the password
         token = server_keypair.createclienttoken(newtoken=True, username=username)
         server_pubkey = base64.b64encode(server_keypair.get_pub_key())
+        # setfromserver is a misnomer not save as serverkeys !!!!!
+        show(client_keypair)
+        client_keypair.saveclientonserver(token=token, username=username)
         return jsonify(status='serverkey', server_pubkey=server_pubkey, token=token)
 
-    #print 'test random is %s' % (base64.b64encode(test))
+    # show('test random is %s' % (base64.b64encode(test))
 
     rec = Record()
     if DBGREQUEST:
-        print "\n\n\n\n\nForm"
+        show("\n\n\n\n\nForm")
         pprint(request.form)
-        print "\nValues"
+        show("\nValues")
         pprint(request.values)
-
-        print "\nRequest Data"
+        show("\nRequest Data")
         pprint(request.data)
 
     data = json.loads(request.data)
     netdata = {'rrsets': data}
     if DBGDATA:
-        print 'request.data is type %s' % (type(data))
-        print "pprint of netdata is :\n%s" % data
+        show('request.data is type %s' % (type(data)))
+        show("print of netdata is :\n%s" % (data))
         pprint(data)
-        print "\n\n"
-        print "pprint of netdata is :\n%s" % netdata
+        show("\n\n")
+        show("print of netdata is :\n%s" % (netdata))
         pprint(netdata)
-        print "netdata is :\n%s" % netdata
+        show("netdata is :\n%s" % (netdata))
 
     retval = rec.api_serverconnect('spotx.tv', netdata)
     return jsonify(retval=retval)
